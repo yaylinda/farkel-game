22442af762f75f7bfbdb46a31093084b84c33343
yaylinda <lindazheng1993@gmail.com>
2020-04-14 01:09:48 -0500

    committing local wip

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,32 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <a href="rules.pdf" target="_blank">Rules</a>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+
+  public $material: any;
+
+  constructor() {
+    super();
+    console.log('App constructor');
+    console.log(`Running in ${process.env.NODE_ENV}`);
+  }
+
+  public mounted() {
+    console.log('App mounted');
+  }
+
+}
+</script>
 
 <style lang="scss">
 #app {
